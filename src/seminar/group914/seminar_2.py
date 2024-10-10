10ae1d66b3c9289db40711b1f16a8f78bac10626
MarchisPatrick04 <patrick.marchis@stud.ubbcluj.ro>
2024-10-10 10:29:27 +0300

    Problem statement whith group 914

---
+++ src/seminar/group914/seminar_2.py
@@ -0,0 +1,18 @@
+"""
+What we want to do today?
+    1. Buuild a simple menu with console
+    2. Work with python's list and dict types
+    3. Generate some random entities in the program (brcause we don't like to type too much)
+    4. How to structure this small program :)
+
+Problem statement:
+    -- We want to manage a list o cities. Each city has a name, population, country and continent
+What the program needs to do (requirements):
+    1. Sort the cities
+    2. Display the list of cities
+    3. Search a list of cities (using partial, case-insensitive str matching)
+    4. Add a city from the console
+    5. Add a number of random cities to the list (the number is read from the console)
+    6. Quit
+"""
+print("Hello cities!")
