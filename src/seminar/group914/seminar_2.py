54a71f572a45fd7b87c597868a9c3fd75690fcd6
MarchisPatrick04 <patrick.marchis@stud.ubbcluj.ro>
2024-10-10 16:07:44 +0300

    Problem statement whith group 914 (update)

--- src/seminar/group914/seminar_2.py
+++ src/seminar/group914/seminar_2.py
@@ -15,4 +15,103 @@ What the program needs to do (requirements):
     5. Add a number of random cities to the list (the number is read from the console)
     6. Quit
 """
-print("Hello cities!")
+
+# --- Functions dealing with how cities aee represented
+def create_city(name: str, population: int, country: str, continent: str):
+    # City represented as a Python list
+    # return [name, population, country, continent]
+
+    # City represented as a Python dict
+    # dict is a key-to-value mapping where keys are unique
+    return {"name": name, "population": population, "country": country, "continent": continent}
+
+def get_name(city) -> str:
+    # for the list representation
+    # return city[0]
+    # for the dict representation
+    return city["name"]
+
+def get_population(city) -> int:
+    # for the list representation
+    # return population[1]
+    # for the dict representation
+    return city["population"]
+
+def get_country(city) -> str:
+    # for the list representation
+    # return country[2]
+    # for the dict representation
+    return city["country"]
+
+def get_continent(city) -> str:
+    # for the list representation
+    # return continent[3]
+    # for the dict representation
+    return city["continent"]
+
+def to_str(city) -> str:
+    return (get_name(city) + " with a population of " + str(get_population(city)) + " is in " + get_country(city) + ", " +
+            get_continent(city))
+
+# --- Functions that implement program requirements
+
+
+
+# --- User interface functions
+# NOTE ALL print(), input() statements go here
+
+def add_city(city_list: list) -> None:
+    print("Adding a new city")
+    name = input("City name =")
+    while True:
+        try:
+            pop = int(input("City population ="))
+            break # If we get to this point, it means there was no error. Try it!
+        except ValueError:
+        print("Population must be an integer")
+    country = input("Country =")
+    continent = input("Continent =")
+
+    # TODO What to do with duplicate cities?
+    new_city = create_city(name, pop, country, continent)
+    city_list.append(new_city)
+
+
+def display_all_cities(city_list: list) -> None:
+    for city in city_list:
+        print(to_str(city))
+
+
+def start():
+    # NOTE We don't want to start with an empty list, so let's add something
+
+    # This is where we keep all the cities
+    # It's not a global variable
+    cities_list = []
+
+    cities_list.append(create_city( "Tulcea", 65000, "Romania", "Europe"))
+    cities_list.append(create_city( "Ploiesti", 80000, "Romania", "Europe"))
+
+    while True:
+        print("1. Display the list of cities")
+        print("0. Quit")
+
+        command = input(">").strip()
+
+        if command == "1":
+            # We want to add a function for each requirement
+            # This makes the main loop easier to understand
+            display_all_cities(cities_list)
+        elif command == "2":
+            add_city(cities_list) # Here I forgot what the professor said to write
+        elif command == "0":
+            break
+        else:
+            print("Bad command or bad file name")
+
+
+start()
+
+# my_city = create_city( "Tulcea", 65000, "Romania", "Europe")
+# print(get_name(my_city), get_population(my_city))
+# print(to_str(my_city))
